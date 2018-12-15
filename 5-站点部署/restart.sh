9b64213db4838999b37e69303f7f9db41aa37b57
jason19839999 <41982395+jason19839999@users.noreply.github.com>
2018-12-15 20:07:30 +0800

    Update restart.sh

--- 5-站点部署/restart.sh
+++ 5-站点部署/restart.sh
@@ -3,6 +3,7 @@
 ps -ef | grep com.juniorchina.Application | grep -v grep| awk '{print $2}' | xargs kill -9
 nohup java -cp .:./config/:./lib/* -Dspring.profiles.active=test-2 com.juniorchina.Application >> serving.log 2>&1 &
 
+在家里配置站点，修改了环境变量还是不行最后用 nohup java -jar demo-0.0.1-SNAPSHOT.jar > demo.log &
 
 注意：***踩过的坑  Error: Could not find or load main class com.imooc.demo.GirlApplication
     查看jdk安装目录： ls -lrt /etc/alternatives/java  https://blog.csdn.net/hjm6507307/article/details/80723280
